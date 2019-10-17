5370aaa6c59ef01eb9f06393f774e424a936a746
Kobe-Ng <kobeng@live.ca>
2019-10-17 16:47:54 +0900

    moved orange juice sims into separate folder, added traps.py

---
+++ 100% orange juice sims/traps.py
@@ -0,0 +1,27 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from functools import reduce
+
+distances = 16
+trap_proced = np.array([0] * distances)
+trials = 50000
+
+for initial_distance in range(1, distances):
+    for _ in range(0, trials):
+        distance_from_trap = initial_distance
+        while distance_from_trap > 0:
+            distance_from_trap -= np.random.randint(1, 7)
+            if distance_from_trap == 0:
+                trap_proced[initial_distance] += 1
+
+probability_trap_procced = list(map(lambda x: x * 100.0 / trials, trap_proced))
+
+print(probability_trap_procced)
+print("the average is:", reduce(lambda a, b: a + b,
+             probability_trap_procced) / len(probability_trap_procced))
+
+plt.ylim(0, 100)
+plt.scatter(list(range(0, distances)), probability_trap_procced)
+plt.xlabel('initial distance from trap')
+plt.ylabel('probability trap was procced while passing over')
+plt.show()
